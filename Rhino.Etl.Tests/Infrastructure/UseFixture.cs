65b00eb06373fb035d8beda2e28136836e4b724d
Gerke Geurts <ggeurts@geurtsrus.com>
2013-12-12 22:20:33 +0100

    Support ADO.NET compatible use of invariant provider names for connections

---
+++ Rhino.Etl.Tests/Infrastructure/UseFixture.cs
@@ -0,0 +1,47 @@
+﻿namespace Rhino.Etl.Tests.Infrastructure
+{
+    using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+    using Rhino.Etl.Core.Infrastructure;
+    using Xunit;
+
+    public class UseFixture
+    {
+        [Fact]
+        public void SupportsAssemblyQualifiedConnectionTypeNameAsProviderNameInConnectionStringSettings()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            ConnectionStringSettings connectionStringSettings = new ConnectionStringSettings("test2", connectionString, typeof(SqlConnection).AssemblyQualifiedName);
+
+            using (IDbConnection connection = Use.Connection(connectionStringSettings))
+            {
+                Assert.NotNull(connection);
+            }
+        }
+ 
+        [Fact]
+        public void SupportsProviderNameInConnectionStringSettings()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            ConnectionStringSettings connectionStringSettings = new ConnectionStringSettings("test2", connectionString, "System.Data.SqlClient");
+
+            using (IDbConnection connection = Use.Connection(connectionStringSettings))
+            {
+                Assert.NotNull(connection);
+            }
+        }
+
+        [Fact]
+        public void UsesSqlClientAsDefaultProviderInConnectionStringSettings()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            ConnectionStringSettings connectionStringSettings = new ConnectionStringSettings("test2", connectionString);
+
+            using (IDbConnection connection = Use.Connection(connectionStringSettings))
+            {
+                Assert.NotNull(connection);
+            }
+        }
+    }
+}
